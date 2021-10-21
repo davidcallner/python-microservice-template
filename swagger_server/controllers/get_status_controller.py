25b091990d169465ee0477e7d37c7fee2698ada3
Prerit Mohan <prerit.mohan@inadev.com>
2021-10-21 12:21:12 -0400

    initial code commit

---
+++ swagger_server/controllers/get_status_controller.py
@@ -0,0 +1,16 @@
+import connexion
+import six
+
+from swagger_server.models.record import Record  # noqa: E501
+from swagger_server import util
+
+
+def get_status():  # noqa: E501
+    """Gets the status
+
+     # noqa: E501
+
+
+    :rtype: Record
+    """
+    return 'do some magic!'
