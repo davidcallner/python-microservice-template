7d7e6cf929b76928e7987a2eeeea6c9b395f519f
Adam Cheslow <adamcheslow@Admins-MacBook-Pro-2.local>
2021-11-04 13:00:46 -0400

    Updating to status_controller.py file

---
+++ swagger_server/controllers/get_status_controller.py
@@ -0,0 +1,13 @@
+import connexion
+import six
+
+from swagger_server.models.record_status import RecordStatus  # noqa: E501
+from swagger_server import util
+
+
+def get_status():  # noqa: E501
+    """Gets the status
+     # noqa: E501
+    :rtype: Record
+    """
+    return 'do some magic!'
